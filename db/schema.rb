39c544fe0ed3fad0006c528cddf972bb7c70c525
iarunpaul <iarunpaul@yahoo.com>
2020-06-05 19:49:42 +0530

    Successfully added comments posting, needs comments display on posting

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200604195159) do
+ActiveRecord::Schema.define(version: 20200605081243) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "commenter"
+    t.integer  "article_id"
   end
 
 end
