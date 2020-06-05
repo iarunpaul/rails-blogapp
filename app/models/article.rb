9254040a73e9887df2f9c716670c68465d2ef182
iarunpaul <iarunpaul@yahoo.com>
2020-06-05 14:07:24 +0530

    Added Comment model and migrated; works on comments form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
 	validates :title, presence: true
 	validates :content, length: {minimum: 5}
+	has_many :comments
 
 end
---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ApplicationRecord
+	validates :commenter, presence: true
+	belongs_to :article
+
+end
